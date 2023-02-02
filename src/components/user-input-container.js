fd328169021ed50eb6e13bfaf4c63eecd270d3b5
MichaelABryant <m.alexander.bryant@gmail.com>
2023-02-01 19:32:54 -0800

    Add structure to user input container

--- src/components/user-input-container.js
+++ src/components/user-input-container.js
@@ -5,6 +5,17 @@ import { WorkExperience } from './work-experience';
 
 class UserInput extends React.Component {
 
+    render() {
+        return (
+            <div>
+                <GeneralInfo />
+                <Education />
+                <button>Add More Education</button>
+                <WorkExperience />
+                <button>Add More Work Experience</button>
+            </div>
+        )
+    }
 }
 
 export { UserInput };
