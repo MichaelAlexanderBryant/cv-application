80ee9867ef42a2558dae65b03fc6340174e2dec4
MichaelABryant <m.alexander.bryant@gmail.com>
2023-02-01 19:06:11 -0800

    Add component files and structure to App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
-import './App.css';
+import React, { useState } from "react";
+import Header from './components/user-input';
+import UserInput from './components/user-input';
+import ResumePreview from './components/resume-preview'
 
 function App() {
+  const [query, setQuery] = useState("");
   return (
-    <div></div>
+    <div>
+      <Header />
+      <div>
+        <UserInput onQuery={setQuery} />
+        <ResumePreview query={query}/>
+      </div>
+    </div>
   );
 }
 
