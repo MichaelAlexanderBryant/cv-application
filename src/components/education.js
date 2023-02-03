cf91aac5a61218ddbd2b475367690bc7ee64cf61
MichaelABryant <m.alexander.bryant@gmail.com>
2023-02-02 18:43:59 -0800

    Fix form overflow issue

--- src/components/education.js
+++ src/components/education.js
@@ -4,22 +4,20 @@ class Education extends React.Component {
 
     render() {
         return (
-            <div>
-                <form id="education-input">
+            <form id="education-input">
                 <span className="input-field">
-                        <label htmlFor="school-name">School Name</label>
-                        <input type="text" name="name"></input>
-                    </span>
-                    <span className="input-field">
-                        <label htmlFor="degree">Degree</label>
-                        <input type="text" name="degree"></input>
-                    </span>
-                    <span className="input-field">
-                        <label htmlFor="attend-date">Date</label>
-                        <input type="text" name="attend-date"></input>
-                    </span>
-                </form>
-            </div>
+                    <label htmlFor="school-name">School Name</label>
+                    <input type="text" name="name"></input>
+                </span>
+                <span className="input-field">
+                    <label htmlFor="degree">Degree</label>
+                    <input type="text" name="degree"></input>
+                </span>
+                <span className="input-field">
+                    <label htmlFor="attend-date">Date</label>
+                    <input type="text" name="attend-date"></input>
+                </span>
+            </form>
         )
     }
 }
