c23811421b91eeb6d16ab0f47e5ffb9df98df743
MichaelABryant <m.alexander.bryant@gmail.com>
2023-02-04 16:54:49 -0800

    Convert WorkExpContainer class component to functional component

--- src/components/work-experience-container.js
+++ src/components/work-experience-container.js
@@ -1,19 +1,17 @@
 import React from 'react';
 import { WorkExperience } from './work-experience';
 
-class WorkExperienceContainer extends React.Component {
 
-    render() {
-        return (
-            <div>
-                <h3 className={ !(this.props.previewOn)  ? "h3-grey" : ((this.props.numberOfWorkComponents > 0) ? "h3-black" : "hide")}>
-                    Work Experience
-                </h3>
-                {Array.from(Array(this.props.numberOfWorkComponents)).map((x, index) => <WorkExperience key={index} previewOn={this.props.previewOn}/>)}
+function WorkExperienceContainer(props) {
 
-            </div>
-        )
-    }
+    return (
+        <div>
+            <h3 className={ !(props.previewOn)  ? "h3-grey" : ((props.numberOfWorkComponents > 0) ? "h3-black" : "hide")}>
+                Work Experience
+            </h3>
+            {Array.from(Array(props.numberOfWorkComponents)).map((x, index) => <WorkExperience key={index} previewOn={props.previewOn}/>)}
+        </div>
+    )
 }
 
 export { WorkExperienceContainer };
