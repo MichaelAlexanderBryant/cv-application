6b1dfa5185a11dd35901e0e51072846430d3a531
MichaelABryant <m.alexander.bryant@gmail.com>
2023-02-04 16:35:05 -0800

    Convert WorkExperience class component to functional component

--- src/components/work-experience.js
+++ src/components/work-experience.js
@@ -1,84 +1,132 @@
-import React from 'react';
+import React, {useState} from 'react';
 
-class WorkExperience extends React.Component {
+function WorkExperience(props) {
 
-    constructor() {
-        super();
-        this.state = {
-            companyNameInput: "",
-            positionInput: "",
-            tasksInput: "",
-            startDateInput: "",
-            endDateInput: ""
-        };
+    const [companyNameInput, setCompanyNameInput] = useState("");
+    const [positionInput, setPositionInput] = useState("");
+    const [tasksInput, setTasksInput] = useState("");
+    const [startDateInput, setStartDateInput] = useState("");
+    const [endDateInput, setEndDateInput] = useState("");
 
-        this.getCompanyName = this.getCompanyName.bind(this);
-        this.getPosition = this.getPosition.bind(this);
-        this.getTasks = this.getTasks.bind(this);
-        this.getStartDate = this.getStartDate.bind(this);
-        this.getEndDate = this.getEndDate.bind(this);
-    };
-
-    getCompanyName(event) {
-        this.setState({companyNameInput: event.target.value})
-    };
-
-    getPosition(event) {
-        this.setState({positionInput: event.target.value})
-    };
-
-    getTasks(event) {
-        this.setState({tasksInput: event.target.value})
-    };
-
-    getStartDate(event) {
-        this.setState({startDateInput: event.target.value})
-    };
-
-    getEndDate(event) {
-        this.setState({endDateInput: event.target.value})
-    };
-
-    render() {
-        return (
-            <div>
-                <div className={this.props.previewOn ? "hide" : ""}>
-                    <form id="work-input">
-                        <span className="input-field">
-                            <label htmlFor="company-name">Company Name</label>
-                            <input type="text" name="company-name" onChange={this.getCompanyName}></input>
-                        </span>
-                        <span className="input-field">
-                            <label htmlFor="position">Position Title</label>
-                            <input type="text" name="position" onChange={this.getPosition}></input>
-                        </span>
-                        <span className="input-field">
-                            <label htmlFor="tasks">Main Tasks</label>
-                            <textarea type="text" name="tasks" onChange={this.getTasks}></textarea>
-                        </span>
-                        <span className="input-field">
-                            <label htmlFor="start-date">Start Date</label>
-                            <input type="text" name="start-date" onChange={this.getStartDate}></input>
-                        </span>
-                        <span className="input-field">
-                            <label htmlFor="end-date">End Date</label>
-                            <input type="text" name="end-date" onChange={this.getEndDate}></input>
-                        </span>
-                    </form>
-                </div>
-                <div className={this.props.previewOn ? "work-entry":"hide"}>
-                    <div className="education-first-line">
-                        <span>
-                            <span className="company-name-preview">{this.state.companyNameInput}</span> | 
-                            <span className="position-preview"> {this.state.positionInput}</span>
-                        </span>
-                        <span className="date-preview">{this.state.startDateInput} - {this.state.endDateInput}</span>
-                    </div>
-                    <div className="tasks-preview">{this.state.tasksInput}</div>
-                </div>
+    return (
+        <div>
+            <div className={props.previewOn ? "hide" : ""}>
+                <form id="work-input">
+                    <span className="input-field">
+                        <label htmlFor="company-name">Company Name</label>
+                        <input type="text" name="company-name" onChange={(event) => setCompanyNameInput(event.target.value)}></input>
+                    </span>
+                    <span className="input-field">
+                        <label htmlFor="position">Position Title</label>
+                        <input type="text" name="position" onChange={(event) => setPositionInput(event.target.value)}></input>
+                    </span>
+                    <span className="input-field">
+                        <label htmlFor="tasks">Main Tasks</label>
+                        <textarea type="text" name="tasks" onChange={(event) => setTasksInput(event.target.value)}></textarea>
+                    </span>
+                    <span className="input-field">
+                        <label htmlFor="start-date">Start Date</label>
+                        <input type="text" name="start-date" onChange={(event) => setStartDateInput(event.target.value)}></input>
+                    </span>
+                    <span className="input-field">
+                        <label htmlFor="end-date">End Date</label>
+                        <input type="text" name="end-date" onChange={(event) => setEndDateInput(event.target.value)}></input>
+                    </span>
+                </form>
             </div>
-        )
-    }
+            <div className={props.previewOn ? "work-entry":"hide"}>
+                <div className="education-first-line">
+                    <span>
+                        <span className="company-name-preview">{companyNameInput}</span> | 
+                        <span className="position-preview"> {positionInput}</span>
+                    </span>
+                    <span className="date-preview">{startDateInput} - {endDateInput}</span>
+                </div>
+                <div className="tasks-preview">{tasksInput}</div>
+            </div>
+        </div>
+    )
 }
+
+// class WorkExperience extends React.Component {
+
+//     constructor() {
+//         super();
+//         this.state = {
+//             companyNameInput: "",
+//             positionInput: "",
+//             tasksInput: "",
+//             startDateInput: "",
+//             endDateInput: ""
+//         };
+
+//         this.getCompanyName = this.getCompanyName.bind(this);
+//         this.getPosition = this.getPosition.bind(this);
+//         this.getTasks = this.getTasks.bind(this);
+//         this.getStartDate = this.getStartDate.bind(this);
+//         this.getEndDate = this.getEndDate.bind(this);
+//     };
+
+//     getCompanyName(event) {
+//         this.setState({companyNameInput: event.target.value})
+//     };
+
+//     getPosition(event) {
+//         this.setState({positionInput: event.target.value})
+//     };
+
+//     getTasks(event) {
+//         this.setState({tasksInput: event.target.value})
+//     };
+
+//     getStartDate(event) {
+//         this.setState({startDateInput: event.target.value})
+//     };
+
+//     getEndDate(event) {
+//         this.setState({endDateInput: event.target.value})
+//     };
+
+//     render() {
+//         return (
+//             <div>
+//                 <div className={this.props.previewOn ? "hide" : ""}>
+//                     <form id="work-input">
+//                         <span className="input-field">
+//                             <label htmlFor="company-name">Company Name</label>
+//                             <input type="text" name="company-name" onChange={this.getCompanyName}></input>
+//                         </span>
+//                         <span className="input-field">
+//                             <label htmlFor="position">Position Title</label>
+//                             <input type="text" name="position" onChange={this.getPosition}></input>
+//                         </span>
+//                         <span className="input-field">
+//                             <label htmlFor="tasks">Main Tasks</label>
+//                             <textarea type="text" name="tasks" onChange={this.getTasks}></textarea>
+//                         </span>
+//                         <span className="input-field">
+//                             <label htmlFor="start-date">Start Date</label>
+//                             <input type="text" name="start-date" onChange={this.getStartDate}></input>
+//                         </span>
+//                         <span className="input-field">
+//                             <label htmlFor="end-date">End Date</label>
+//                             <input type="text" name="end-date" onChange={this.getEndDate}></input>
+//                         </span>
+//                     </form>
+//                 </div>
+//                 <div className={this.props.previewOn ? "work-entry":"hide"}>
+//                     <div className="education-first-line">
+//                         <span>
+//                             <span className="company-name-preview">{this.state.companyNameInput}</span> | 
+//                             <span className="position-preview"> {this.state.positionInput}</span>
+//                         </span>
+//                         <span className="date-preview">{this.state.startDateInput} - {this.state.endDateInput}</span>
+//                     </div>
+//                     <div className="tasks-preview">{this.state.tasksInput}</div>
+//                 </div>
+//             </div>
+//         )
+//     }
+// }
 
 export { WorkExperience };
