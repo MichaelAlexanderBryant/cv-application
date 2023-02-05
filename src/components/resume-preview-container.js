d3daebfc85cceaa45302360d23f218c488558a2d
MichaelABryant <m.alexander.bryant@gmail.com>
2023-02-04 16:57:21 -0800

    Convert ResumePreview class component to functional component

--- src/components/resume-preview-container.js
+++ src/components/resume-preview-container.js
@@ -1,17 +1,15 @@
 import React from 'react';
 
-class ResumePreview extends React.Component {
+function ResumePreview() {
 
-    render() {
-        return (
-            <div id="right-panel">
-                <h2>
-                    CV Preview
-                </h2>
-                <div id="on-paper-preview"></div>
-            </div>
-        )
-    }
+    return (
+        <div id="right-panel">
+            <h2>
+                CV Preview
+            </h2>
+            <div id="on-paper-preview"></div>
+        </div>
+    )
 }
 
 export { ResumePreview };
