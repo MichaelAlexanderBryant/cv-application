8fedfbf85377240b776b7b76934c567337f7f28c
MichaelABryant <m.alexander.bryant@gmail.com>
2023-02-04 16:53:10 -0800

    Convert EduContainer class component to functional component

--- src/components/education-container.js
+++ src/components/education-container.js
@@ -1,19 +1,16 @@
 import React from 'react';
 import { Education } from './education';
 
-class EducationContainer extends React.Component {
+function EducationContainer(props) {
 
-    render() {
-        return (
-            <div>
-                {/* <h3 className={this.props.previewOn ? "hide" : ""}> */}
-                <h3 className={ !(this.props.previewOn)  ? "h3-grey" : ((this.props.numberOfEducationComponents > 0) ? "h3-black" : "hide")}>
-                    Education
-                </h3>
-                {Array.from(Array(this.props.numberOfEducationComponents)).map((x, index) => <Education key={index} previewOn={this.props.previewOn}/>)}
-            </div>
-        )
-    }
+    return (
+        <div>
+            <h3 className={ !(props.previewOn)  ? "h3-grey" : ((props.numberOfEducationComponents > 0) ? "h3-black" : "hide")}>
+                Education
+            </h3>
+            {Array.from(Array(props.numberOfEducationComponents)).map((x, index) => <Education key={index} previewOn={props.previewOn}/>)}
+        </div>
+    )
 }
 
 export { EducationContainer };
